9d4543d230c2309c509c7768ac6605844d220585
Ana <anitalucenahayward@gmail.com>
2022-10-03 00:34:03 -0300

    hice ejercicio 2 de react

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Ejercicio1 from "./componentes/Ejercicio1";
+import Ejercicio2 from "./componentes/Ejercicio2";
 
 
 function App() {
   //aqui va la mayoria de la logica
+const amigo = 'my friend!' ;
 
 
   return (
     <>
     <Ejercicio1></Ejercicio1>
+    <Ejercicio2 codigoAmigo={amigo} estado={true}></Ejercicio2>
     </>
   );
 }
---
+++ src/componentes/Ejercicio2.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Ejercicio2 = (props) => {
+    return (
+        <div>
+            <h2>Hello {props.codigoAmigo}</h2>
+        </div>
+    );
+};
+
+export default Ejercicio2;
