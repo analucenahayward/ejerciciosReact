4646904390d32e566727d64b8657f0d785a9d8f0
Ana <anitalucenahayward@gmail.com>
2022-10-03 00:19:59 -0300

    hice ejercicio 1 de react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Ejercicio1 from "./componentes/Ejercicio1";
+
+
+function App() {
+  //aqui va la mayoria de la logica
+
+
+  return (
+    <>
+    <Ejercicio1></Ejercicio1>
+    </>
+  );
+}
+
+export default App;
